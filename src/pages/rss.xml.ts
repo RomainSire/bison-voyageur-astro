a827a7a790b2707a0201f901d894546c964bcb83
Romain <rsire@tuta.io>
2024-03-29 00:14:58 +0900

    feat(rss): add automatic RSS feed

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems, type RSSFeedItem } from '@astrojs/rss';
+
+export async function GET(context: { site: string | URL }) {
+	return rss({
+		title: 'Astro Learner | Blog',
+		description: 'My journey learning Astro',
+		site: context.site,
+		items: (await pagesGlobToRssItems(import.meta.glob('./**/*.md'))) as RSSFeedItem[],
+		customData: `<language>fr</language>`,
+	});
+}
